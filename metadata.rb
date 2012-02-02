d9a28d044664592c5ac97a4f728b75b9515ef469
Jamie Winsor <jamie@vialstudios.com>
2012-02-02 13:31:03 -0800

    bump 0.9.2

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ maintainer       "Riot Games"
 maintainer_email "jwinsor@riotgames.com"
 license          "Apache 2.0"
 description      "Installs and configures redis"
-version          "0.9.1"
+version          "0.9.2"
 
 supports "centos", ">= 5.5"
